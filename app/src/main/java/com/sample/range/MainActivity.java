b57a31a4783809e26ea12e576170c635060abcee
ljhymca <ljhymca@naver.com>
2020-04-26 02:15:29 +0900

    위도 경도 출력 추가

--- app/src/main/java/com/sample/range/MainActivity.java
+++ app/src/main/java/com/sample/range/MainActivity.java
@@ -1,14 +1,20 @@
 package com.sample.range;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.Utmk;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -19,14 +25,38 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         MapFragment mapFragment= (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+
+        }
+
+        Utmk utmk = new Utmk(953935,1952044.1);
+        LatLng latLng = utmk.toLatLng();
+
+        Toast.makeText(this,"위도: "+latLng.latitude + "경도: " +latLng.longitude,
+                Toast.LENGTH_LONG).show();
+
+
     }
 
-    @Override
+
+//    @UiThread
+//    @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-        //FusedLocationSource locationSource = new FusedLocationSource{this, 100};
-        //naverMap.setLocationSource(locationSource);
-        //UiSettings uiSettings = naverMap.getUiSettings();
-        //uiSettings.setLocationButtonEnabled(true);
-    }
+
+
+        
+
+
+        //        FusedLocationSource locationSource = new FusedLocationSource{this, 100};
+//        naverMap.setLocationSource(locationSource);
+//        UiSettings uiSettings = naverMap.getUiSettings();
+//        uiSettings.setLocationButtonEnabled(true);
+//        Marker marker2 = new Marker();
+//        marker2.setPosition(new LatLng(37.763695, 127.281796));
+//        marker2.setMap(naverMap);
+//
+   }
 }
