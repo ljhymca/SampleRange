f5b640084ee3aeab1e60d1583a4e0435fcb248e6
ljhymca <ljhymca@naver.com>
2020-04-27 01:40:07 +0900

    화면 클릭시 현재 위도 경도 출력

--- app/src/main/java/com/sample/range/MainActivity.java
+++ app/src/main/java/com/sample/range/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapFragment mapFragment= (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        MapFragment mapFragment= (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);//지도 객체 생성
 
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance();
@@ -33,11 +33,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         mapFragment.getMapAsync(this);
 
-        Utmk utmk = new Utmk(953935,1952044.1);
-        LatLng latLng = utmk.toLatLng();
-
-        Toast.makeText(this,"위도: "+latLng.latitude + "경도: " +latLng.longitude,
-                Toast.LENGTH_LONG).show();
+//        Utmk utmk = new Utmk(953935,1952044.1);
+//        LatLng latLng = utmk.toLatLng();
+//
+//        Toast.makeText(this,"위도: "+latLng.latitude + "경도: " +latLng.longitude,
+//                Toast.LENGTH_LONG).show();//들어갔을때 위도 경도 표시
 
 
     }
@@ -45,19 +45,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-        naverMap.setOnMapClickListener((point, coord) ->
-            Toast.makeText(this, getString(R.string.format_map_click, coord.latitude, coord.longitude), Toast.LENGTH_SHORT).show());
+        Marker marker2 = new Marker();
+        marker2.setPosition(new LatLng(37.763695, 126.281796));
+        marker2.setMap(naverMap);
 
-        
+        naverMap.setOnMapClickListener((point, coord)->  //지도 화면클릭시
+                Toast.makeText(this, getString(R.string.format_map_click, coord.latitude, coord.longitude), Toast.LENGTH_SHORT).show());
 
-
-        //        FusedLocationSource locationSource = new FusedLocationSource{this, 100};
+//        FusedLocationSource locationSource = new FusedLocationSource(this, 100);
 //        naverMap.setLocationSource(locationSource);
 //        UiSettings uiSettings = naverMap.getUiSettings();
 //        uiSettings.setLocationButtonEnabled(true);
-//        Marker marker2 = new Marker();
-//        marker2.setPosition(new LatLng(37.763695, 127.281796));
-//        marker2.setMap(naverMap);
-//
-   }
+
+    }
 }
