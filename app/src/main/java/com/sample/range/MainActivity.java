d477c16ead247df4a2ce2ce6c985be2bb5747e4f
ljhymca <ljhymca@naver.com>
2020-05-14 16:40:54 +0900

    firebase연동(현재시간 기준 업로드)

--- app/src/main/java/com/sample/range/MainActivity.java
+++ app/src/main/java/com/sample/range/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -26,12 +29,16 @@ import com.naver.maps.map.overlay.CircleOverlay;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     SeekBar sizeBar;
     TextView sizeBarView;
     Button sendbutton;
 
+    private static Handler mHandler;
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
@@ -39,8 +46,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         MapFragment mapFragment= (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);//지도 객체 생성
 
@@ -65,24 +70,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         marker2.setPosition(new LatLng(1, 1));//일단 마커 위치지정
         marker2.setMap(naverMap);//마커 생성
         CircleOverlay markerCircle = new CircleOverlay();//원 생성
-
+        //SeekBar할당
+        int abc = 500;
 
 
         naverMap.setOnMapClickListener((point, coord)-> { //지도 화면클릭시
             Toast.makeText(this, getString(R.string.format_map_click, coord.latitude, coord.longitude), Toast.LENGTH_SHORT).show();
             marker2.setPosition(new LatLng(coord.latitude, coord.longitude));//클릭 좌표로 마커 위치 이동
             markerCircle.setCenter(coord);//원 중심을 선택한 위치로
-            markerCircle.setRadius(500);//원 반지름
+            markerCircle.setRadius(abc);//원 반지름
             markerCircle.setColor(0x4000FFFF);//불투명도 지정
             markerCircle.setMap(naverMap);
-
-
             sendbutton = (Button) findViewById(R.id.sendButton);
+            long now = System.currentTimeMillis();//시간생성
+            Date date = new Date(now);
+            SimpleDateFormat Now = new SimpleDateFormat("yyyyMMddHHmmss");
+            String formatDate = Now.format(date);
 
             sendbutton.setOnClickListener(new Button.OnClickListener(){
                 public void onClick(View v){//버튼 클릭시 현재 위도 경도 값 전송
-                    databaseReference.child("latitude").push().setValue(coord.latitude);//위도
-                    databaseReference.child("longitude").push().setValue(coord.longitude);//경도
+                    databaseReference.child(formatDate).child("latitude").push().setValue(coord.latitude);//위도
+                    databaseReference.child(formatDate).child("longitude").push().setValue(coord.longitude);//경도
+                    //원 범위
+                    //id 값 넣을 자리
                 }
             });
         });
@@ -91,6 +101,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 //        naverMap.setLocationSource(locationSource);
 //        UiSettings uiSettings = naverMap.getUiSettings();
 //        uiSettings.setLocationButtonEnabled(true);
+
 
     }
 
